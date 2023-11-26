41eab4346cff953224e5079244b4ff05bacbda8b
Pau Estalella <pau.estalella@gmail.com>
2023-11-26 11:17:20 +0100

    Use println instead of Akka logging
    Akka loggers are waaay too verbose to be useful for this use-case.

--- FitnessEvaluatorRemote/src/main/scala/FitnessEvaluatorActor.scala
+++ FitnessEvaluatorRemote/src/main/scala/FitnessEvaluatorActor.scala
@@ -22,18 +22,18 @@ class FitnessEvaluatorActor extends Actor {
 			val path = "akka://LocalFitnessSystem@10.0.200.226:5555/user/LocalFitnessRequester"
 			implicit val resolveTimeout: Timeout = Timeout(5 seconds)
 			for (master: ActorRef <- context.actorSelection(path).resolveOne()) {
-				log.info("Found master node")
+				println("Found master node")
 				master ! NodeStartRequest
 			}
 		case NodeStartAcknowledge =>
-			log.info("The master node acknowledged our request")
+			println("The master node acknowledged our request")
 
 		case cmd: EvalCommand =>
-			log.info(s"Got a command with transaction ID = ${cmd.transactionID} and ${cmd.circuits.size} elements to process from ${sender().path.toString}")
+			println(s"Got a command with transaction ID = ${cmd.transactionID} and ${cmd.circuits.size} elements to process from ${sender().path.toString}")
 			val evaluation = runCommand(cmd, sender())
 			evaluation.onComplete {
 				case Success(value) =>
-					log.info(s"Got a result. Sending it to the requester [${value.sender.path.toString}]")
+					println(s"Got a result. Sending it to the requester [${value.sender.path.toString}]")
 					value.sender ! value.result
 				case Failure(t) =>
 					log.warning("FAILED TO GET A RESULT")
@@ -43,14 +43,14 @@ class FitnessEvaluatorActor extends Actor {
 
 	private def runCommand(command: EvalCommand, sender: ActorRef): Future[PendingResponse] = Future {
 		val fitEval = GaiaCommon.LowPassFilter(limitFreq = 5000)
-		log.info("Starting measurement")
+		println("Starting measurement")
 		val measurements = command.circuits.zipWithIndex.par.map(m =>
 			(m._2, eval.calcFitness(m._1, fitEval))).toIndexedSeq
-		log.info("Measurements done")
+		println("Measurements done")
 		//		assert(measurements.foldLeft(true, -1)(
 		//			(last, cur) => (last._1 && (last._2 + 1 == cur._1), cur._1)
 		//		)._1)
-		//		log.info("Results are ordered")
+		//		println("Results are ordered")
 		PendingResponse(
 			result = FitnessResult(
 				transactionID = command.transactionID,
